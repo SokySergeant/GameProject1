af9d29c11ce5485b7ce80e8fe4e0b34dcbbe441d
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2022-10-12 12:54:16 +0200

    Added the EnvironmentData SO.
    • Added a new ScriptableObject called EnvirontmentData for easy configuration of segment pieces.
    • Added a ScriptableObject folder called SOs.

---
+++ Assets/Scripts/EnvironmentData.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(fileName = "EnvironmentData", menuName = "ScriptableObjects/EnvironmentData", order = 1)]
+public class EnvironmentData : ScriptableObject
+{
+    public string Name = "Environment";
+    
+    public SegmentProfile[] SegmentProfiles;
+
+    public float WeightSum;
+    public AnimationCurve WeightCurve;
+    
+    [Serializable]
+    public class SegmentProfile
+    {
+        [Tooltip("Segment Prefab used for instantiation.")]
+        public GameObject Prefab;
+        [Tooltip("Segment MonoBehaviour attached to the Segment Prefab.")]
+        public PathSection Segment;
+        [Tooltip("Weight used when selecting a random Segment from this Environment. Weights are normalized.")]
+        [Range(0f, 1f)] public float Weight = 0.5f;
+
+        public bool Validate()
+        {
+            if (!Prefab) return false;
+
+            if (!Prefab.TryGetComponent(out PathSection segment))
+            {
+                Debug.LogError($"Missing Segment MonoBehaviour on {Prefab.name}'s root.", Prefab);
+
+                return false;
+            }
+
+            Segment = segment;
+            return true;
+        }
+    }
+
+    private void OnValidate()
+    {
+        WeightCurve = new AnimationCurve();
+        WeightSum = 0f;
+        Keyframe key;
+        for (int i = 0; i < SegmentProfiles.Length; i++)
+        {
+            SegmentProfile profile = SegmentProfiles[i];
+            float probability = profile.Weight;
+
+            if (probability > 0f)
+            {
+                key = new Keyframe(WeightSum, i, Mathf.Infinity, Mathf.Infinity);
+                WeightCurve.AddKey(key);
+            }
+
+            WeightSum += probability;
+            
+            profile.Validate();
+        }
+        
+        key = new Keyframe(WeightSum, WeightSum > 0f ? SegmentProfiles.Length-1 : -1, Mathf.Infinity, Mathf.Infinity);
+        WeightCurve.AddKey(key);
+    }
+
+    public int RandomSegmentIndex()
+    {
+        return (int)WeightCurve.Evaluate(Random.Range(0f, 1f));
+    }
+    
+    public GameObject RandomSegmentPrefab()
+    {
+        int profileIndex = RandomSegmentIndex();
+        return profileIndex >= 0 ? SegmentProfiles[profileIndex].Prefab : null;
+    }
+}
