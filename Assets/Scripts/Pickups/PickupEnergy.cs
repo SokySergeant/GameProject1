cec866706608328e006c891bf2f4e2f5fa71e4c5
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2022-10-20 15:36:20 +0200

    Added a Pickup for Energy.
    • Added a new Pickup for Energy, but it is not finished.
    • Added animations for the Hoop prefab.
    • Added an Animations folder.
    • Tweaked the Hoop Shatter particle system.
    • Made a Hoop_Energy Pickup prefab.

---
+++ Assets/Scripts/Pickups/PickupEnergy.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PickupEnergy : PickupAbstract
+{
+    private bool _hasAnimator;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _hasAnimator = TryGetComponent(out _animator);
+        
+        if (_hasAnimator)
+            _animator.SetFloat("Random", Random.Range(-1f, 1f));
+    }
+
+    public override void DoPickupAction(GameManager gameManager){
+
+        if (_hasAnimator)
+        {
+            _animator.SetTrigger("OnPickup");
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    private void OnAnimationEnd()
+    {
+        Destroy(gameObject);
+    }
+}
